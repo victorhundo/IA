4ca377ba21dcf9e5b88c811081fd722a3be0d3d2
victorhundo <victorhundo@gmail.com>
2019-10-22 12:42:27 -0300

    rede bayesiana em robocode

--- IA/HAL9001.java
+++ IA/HAL9001.java
@@ -1,5 +1,12 @@
 package IA;
+import org.apache.commons.math3.distribution.EnumeratedIntegerDistribution;
 import robocode.*;
+import org.apache.commons.math3.*;
+import robocode.Robot;
+
+import java.awt.*;
+import java.util.prefs.Preferences;
+
 //import java.awt.Color;
 
 // API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
@@ -12,23 +19,69 @@ public class HAL9001 extends Robot
     /**
      * run: HAL9001's default behavior
      */
+    private String[] states = new String[]{"gunR", "gunL", "ahead"};
+    private String[][] actions = new String[][]{
+            {"gunR-gunR", "gunR-gunL", "gunR-ahead"},
+            {"gunL-gunR", "gunL-gunL", "gunL-ahead"},
+            {"ahead-gunR", "ahead-gunL", "ahead-ahead"}};
+    private double[][] probMatrix = new double[][]{
+            {0.9, 0.075, 0.025},
+            {0.15, 0.8, 0.05},
+            {0.25, 0.25, 0.5}};
+
+    public void onPaint(Graphics2D g) {
+        // Set the paint color to red
+        g.setColor(java.awt.Color.RED);
+        // Paint a filled rectangle at (50,50) at size 100x150 pixels
+        g.fillRect(50, 50, 100, 150);
+    }
+
+
     public void run() {
-        // Initialization of the robot should be put here
-
-        // After trying out your robot, try uncommenting the import at the top,
-        // and the next line:
-
-        // setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+        String currentState = states[0];
+        String action = null;
 
         // Robot main loop
+        System.out.println(this.probMatrix.length);
         while(true) {
-            // Replace the next 4 lines with any behavior you would like
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
+            int[] indexes = new int[]{0,1,2};
+
+            if ( currentState.equals(states[0]) ){
+                EnumeratedIntegerDistribution dist = new EnumeratedIntegerDistribution(indexes, this.probMatrix[0]);
+                action = actions[0][dist.sample()];
+            }
+
+            else if (currentState.equals(states[1])) {
+                EnumeratedIntegerDistribution dist = new EnumeratedIntegerDistribution(indexes, this.probMatrix[1]);
+                action = actions[1][dist.sample()];
+            }
+
+            else if (currentState.equals(states[2])) {
+
+                EnumeratedIntegerDistribution dist = new EnumeratedIntegerDistribution(indexes, this.probMatrix[2]);
+                action = actions[2][dist.sample()];
+            }
+
+            currentState = fAction(action);
         }
     }
+
+    public String fAction(String action) {
+        String state = "";
+        if ( action.equals(this.actions[0][0]) || action.equals(this.actions[1][0]) || action.equals(this.actions[2][0]) ) {
+            turnGunRight(360);
+            state = this.states[0];
+        }
+        else if (action.equals(this.actions[0][1]) || action.equals(this.actions[1][1]) || action.equals(this.actions[2][1])) {
+            turnGunLeft(360);
+            state = this.states[1];
+        }
+        else if (action.equals(this.actions[0][2]) || action.equals(this.actions[1][2]) || action.equals(this.actions[2][2])) {
+            ahead(100);
+            state = this.states[2];
+        }
+        return state;
+    };
 
     /**
      * onScannedRobot: What to do when you see another robot
